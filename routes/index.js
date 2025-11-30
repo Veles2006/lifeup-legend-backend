dcacf97ab7ee1afafffc2bf239cdc706af8bcf74
Veles2006 <anhphiax147x@gmail.com>
2025-11-30 17:10:28 +0800

    cập nhật sửa lỗi api auth

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ import playerRoutes from './playerRoutes.js';
 import taskRoutes from './taskRoutes.js';
 import itemRoutes from './itemRoutes.js';
 import dictionaryRoutes from './dictionaryRoutes.js'
+import authRoutes from './authRoutes.js'
 
 const router = express.Router();
 
+router.use('/auth', authRoutes);
 router.use('/players', playerRoutes);
 router.use('/tasks', taskRoutes);
 router.use('/items', itemRoutes);
