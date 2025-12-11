6f1df2afed89690deba69a0c76afba65b7f8d160
Veles2006 <anhphiax147x@gmail.com>
2025-12-11 23:14:50 +0800

    thêm model mới: Block

---
+++ models/Block.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+// const conditionScheme = new mongoose.Schema({
+
+// })
+
+// const packageScheme = new mongoose.Schema({
+//     appName: { type: String, require: true },
+//     package: { type: String, require: true },
+//     status: { type: String, },
+//     condition: {
+//         time: String,
+
+//     },
+// })
+
+const blockScheme = new mongoose.Schema(
+    {
+        idUser: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        appName: String,
+        packageName: String,
+        type: {
+            type: String,
+            enum: ['permanent', 'scheduled', 'timer'],
+        },
+        type: [{ start: String, end: String }],
+        default: [],
+        expiresAt: Date,
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Block', blockScheme, 'block');
