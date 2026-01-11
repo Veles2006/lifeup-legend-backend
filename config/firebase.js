546fa538e4cf78928e99fef4ebef34d8cf3dcc3f
Veles2006 <anhphiax147x@gmail.com>
2026-01-11 16:41:29 +0800

    sửa lỗi tương thích với file firebase-adming.

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,17 @@
 import admin from 'firebase-admin';
 import fs from 'fs';
 
+const path =
+    process.env.NODE_ENV === 'production'
+        ? '/etc/secrets/firebase-admin.json'
+        : '../firebase-admin.json';
+
 const serviceAccount = JSON.parse(
-  fs.readFileSync('../firebase-admin.json', 'utf-8')
+    fs.readFileSync(path, 'utf-8')
 );
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
